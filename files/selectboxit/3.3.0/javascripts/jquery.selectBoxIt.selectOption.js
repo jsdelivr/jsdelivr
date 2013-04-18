7fa352d4f34a24ad49ebb4b09d3ed1e561c629b6
Dmitriy Akulov <jimaek@hotmail.com>
2013-04-18 16:48:54 +0300

    selectboxit update

---
+++ files/selectboxit/3.3.0/javascripts/jquery.selectBoxIt.selectOption.js
@@ -0,0 +1,35 @@
+
+    // Select Option Module
+    // ====================
+
+    // Select Option
+    // -------------
+    //      Programatically selects a drop down option by either index or value
+
+    selectBoxIt.selectOption = function(val, callback) {
+
+        // Stores the plugin context inside of the self variable
+        var self = this;
+
+        // Makes sure the passed in position is a number
+        if((typeof val).toLowerCase() === "number") {
+
+            // Set's the original select box value and triggers the change event (which SelectBoxIt listens for)
+            self.selectBox.val(self.selectBox.find("option").eq(val).val()).change();
+
+        }
+
+        else if((typeof val).toLowerCase() === "string") {
+
+            // Set's the original select box value and triggers the change event (which SelectBoxIt listens for)
+            self.selectBox.val(val).change();
+
+        }
+
+        // Calls the callback function
+        self._callbackSupport(callback);
+
+        // Maintains chainability
+        return self;
+
+    };
