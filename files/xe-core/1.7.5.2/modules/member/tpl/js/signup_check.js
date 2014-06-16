f619e62bb56972bb4b1c1cf663d77f1d5c20cec6
ajkj <thename@requery.me>
2014-06-17 04:56:01 +0900

    xe-core added

---
+++ files/xe-core/1.7.5.2/modules/member/tpl/js/signup_check.js
@@ -0,0 +1,5 @@
+/**
+ * @brief 회원 가입시나 정보 수정시 각 항목의 중복 검사등을 하는 기능을 구현
+ * @author NAVER (developer@xpressengine.com)
+ *!*/
+var memberCheckObj={target:null,value:null};jQuery(document).ready(memberSetEvent);function memberSetEvent(){jQuery("#fo_insert_member :input").filter("[name=user_id],[name=nick_name],[name=email_address]").blur(memberCheckValue)}function memberCheckValue(event){var field=event.target;var _name=field.name;var _value=field.value;if(!_name||!_value){return}var params={name:_name,value:_value};var response_tags=["error","message"];exec_xml("member","procMemberCheckValue",params,completeMemberCheckValue,response_tags,field)}function completeMemberCheckValue(ret_obj,response_tags,field){var _id="dummy_check"+field.name;var dummy=jQuery("#"+_id);if(ret_obj.message=="success"){dummy.html("").hide();return}if(!dummy.length){dummy=jQuery('<p class="checkValue help-inline" style="color:red" />').attr("id",_id).appendTo(field.parentNode)}dummy.html(ret_obj.message).show()}function removeMemberCheckValueOutput(dummy,obj){dummy.style.display="none"};
