8132ca045cf5f5e05d8bab63d09cec12d5293868
HIROKAZU uchiyama <sdr0x07b6@gmail.com>
2014-11-11 07:23:22 +0900

    Registered jQuery.inview issues an error. It was replaced.

--- files/jquery.inview/0.1/jquery.inview.js
+++ files/jquery.inview/0.1/jquery.inview.js
@@ -1,21 +1,30 @@
 /**
  * author Remy Sharp
  * url http://remysharp.com/2009/01/26/element-in-view-event-plugin/
+ * fork https://github.com/zuk/jquery.inview
  */
-(function ($) {
-    var shouldCheckInView = false;
+(function($) {
+    'use strict';
+
+    function getScrollTop() {
+        return window.pageYOffset ||
+            document.documentElement.scrollTop ||
+            document.body.scrollTop;
+    }
 
     function getViewportHeight() {
         var height = window.innerHeight; // Safari, Opera
         // if this is correct then return it. iPad has compat Mode, so will
         // go into check clientHeight (which has the wrong value).
-        if (height) { return height; }
+        if (height) {
+            return height;
+        }
         var mode = document.compatMode;
 
-        if ( (mode || !$.support.boxModel) ) { // IE, Gecko
-            height = (mode == 'CSS1Compat') ?
-            document.documentElement.clientHeight : // Standards
-            document.body.clientHeight; // Quirks
+        if ((mode || !$.support.boxModel)) { // IE, Gecko
+            height = (mode === 'CSS1Compat') ?
+                document.documentElement.clientHeight : // Standards
+                document.body.clientHeight; // Quirks
         }
 
         return height;
@@ -26,69 +35,75 @@
         // This works-around iOS < 4 on iPad giving incorrect value
         // cf http://bugs.jquery.com/ticket/6446#comment:9
         var curtop = 0;
-        for (var obj = debug; obj !== null; obj = obj.offsetParent) {
+        for (var obj = debug; obj; obj = obj.offsetParent) {
             curtop += obj.offsetTop;
         }
         return curtop;
     }
 
-    function check_inview() {
-        var vpH = getViewportHeight(),
-            scrolltop = (window.pageYOffset ?
-                window.pageYOffset :
-                document.documentElement.scrollTop ?
-                document.documentElement.scrollTop :
-                document.body.scrollTop),
+    function checkInView() {
+        var viewportTop = getScrollTop(),
+            viewportBottom = viewportTop + getViewportHeight(),
             elems = [];
 
         // naughty, but this is how it knows which elements to check for
-        $.each($.cache, function () {
+        $.each($.cache, function() {
             if (this.events && this.events.inview) {
                 elems.push(this.handle.elem);
             }
         });
 
-        if (elems.length) {
-            $(elems).each(function () {
-                var $el = $(this),
-                    top = offsetTop(this),
-                    height = $el.height(),
-                    inview = $el.data('inview') || false;
+        $(elems).each(function() {
+            var $el = $(this),
+                elTop = offsetTop(this),
+                elHeight = $el.height(),
+                elBottom = elTop + elHeight,
+                wasInView = $el.data('inview') || false,
+                offset = $el.data('offset') || 0,
+                inView = elTop > viewportTop && elBottom < viewportBottom,
+                isBottomVisible = elBottom + offset > viewportTop && elTop < viewportTop,
+                isTopVisible = elTop - offset < viewportBottom && elBottom > viewportBottom,
+                inViewWithOffset = inView || isBottomVisible || isTopVisible ||
+                    (elTop < viewportTop && elBottom > viewportBottom);
 
-                if (scrolltop > (top + height) || scrolltop + vpH < top) {
-                    if (inview) {
-                        $el.data('inview', false);
-                        $el.trigger('inview', [ false ]);
-                    }
-                } else if (scrolltop < (top + height)) {
-                    var visPart = ( scrolltop > top ? 'bottom' : (scrolltop + vpH) < (top + height) ? 'top' : 'both' );
-                    if (!inview || inview !== visPart) {
-                      $el.data('inview', visPart);
-                      $el.trigger('inview', [ true, visPart]);
-                    }
+            if (inViewWithOffset) {
+                var visPart = (isTopVisible) ? 'top' : (isBottomVisible) ? 'bottom' : 'both';
+                if (!wasInView || wasInView !== visPart) {
+                    $el.data('inview', visPart);
+                    $el.trigger('inview', [true, visPart]);
                 }
-            });
+            } else if (!inView && wasInView) {
+                $el.data('inview', false);
+                $el.trigger('inview', [false]);
+            }
+        });
+    }
+
+    function createFunctionLimitedToOneExecutionPerDelay(fn, delay) {
+        var shouldRun = false;
+        var timer = null;
+
+        function runOncePerDelay() {
+            if (timer !== null) {
+                shouldRun = true;
+                return;
+            }
+            shouldRun = false;
+            fn();
+            timer = setTimeout(function() {
+                timer = null;
+                if (shouldRun) {
+                    runOncePerDelay();
+                }
+            }, delay);
         }
+
+        return runOncePerDelay;
     }
 
-    function triggerInViewChecker() {
-        shouldCheckInView = true;
-    }
-
-    $(window).scroll(triggerInViewChecker);
-    $(window).resize(triggerInViewChecker);
-    $(window).click(triggerInViewChecker);
-    // kick the event to pick up any elements already in view.
+    // ready.inview kicks the event to pick up any elements already in view.
     // note however, this only works if the plugin is included after the elements are bound to 'inview'
-    $(window).ready(triggerInViewChecker);
+    var runner = createFunctionLimitedToOneExecutionPerDelay(checkInView, 100);
+    $(window).on('checkInView.inview click.inview ready.inview scroll.inview resize.inview', runner);
 
-    // Check every 250 milliseconds if a scroll/click/resize/ready event is triggered
-    // Source: http://ejohn.org/blog/learning-from-twitter/
-    setInterval(function() {
-        if ( shouldCheckInView ) {
-            shouldCheckInView = false;
-
-            check_inview();
-        }
-    }, 100);
 })(jQuery);
