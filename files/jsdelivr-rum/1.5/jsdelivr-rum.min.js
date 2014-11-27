2b3e58bdd714cdb1f66f3e5acf4a0b64a7d11e3d
Dmitriy Akulov <dakulovgr@gmail.com>
2014-11-27 17:17:12 +0300

    New jsDelivr RUM code

---
+++ files/jsdelivr-rum/1.5/jsdelivr-rum.min.js
@@ -0,0 +1,1 @@
+(function(a,b,c,d,e){function f(){var a=b.createElement("script");a.async=!0;a.src="//radar.cedexis.com/1/11475/radar.js";b.body.appendChild(a)}/\bMSIE 6/i.test(a.navigator.userAgent)||(a[c]?a[c](e,f,!1):a[d]&&a[d]("on"+e,f))})(window,document,"addEventListener","attachEvent","load");
