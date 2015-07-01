283d52e0527e4a4ab08058fa77c7a7836e6bdcc6
Eyal Flato <eyal@cappsool.com>
2015-07-01 16:39:23 +0300

    V0.0.1

---
+++ files/cappsool/V0.0.1/scripts/newWidget.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function(){
+  $('.frappe-slider').slick({
+    centerMode: true,
+    centerPadding: '40px',
+    slidesToShow: 1,
+    autoplay: false,
+    autoplaySpeed:4000,
+    arrows: false
+
+  });
+
+});
+
