ef4e02cf083c2eab06333e31f1ce44e3efb94965
Cheon Sangwon <swcheon@gmail.com>
2015-11-27 15:03:53 +0900

    Revert "Update jquery.datatables to 1.10.10"
    This reverts commit 45d7a7fb5e240bc7c430553ebcc034d046a6c26a.

--- files/jquery.datatables/1.10.10/plugins/buttons/js/buttons.foundation.min.js
+++
@@ -1,6 +0,0 @@
-/*!
- Foundation integration for DataTables' Buttons
- ©2015 SpryMedia Ltd - datatables.net/license
-*/
-(function(c){"function"===typeof define&&define.amd?define(["jquery","datatables.net-zf","datatables.net-buttons"],function(a){return c(a,window,document)}):"object"===typeof exports?module.exports=function(a,b){a||(a=window);if(!b||!b.fn.dataTable)b=require("datatables.net-zf")(a,b).$;b.fn.dataTable.Buttons||require("datatables.net-buttons")(a,b);return c(b,a,a.document)}:c(jQuery,window,document)})(function(c){var a=c.fn.dataTable;c.extend(!0,a.Buttons.defaults,{dom:{container:{tag:"ul",className:"dt-buttons button-group"},
-buttonContainer:{tag:"li",className:""},button:{tag:"a",className:"button small"},buttonLiner:{tag:null},collection:{tag:"ul",className:"dt-button-collection f-dropdown open",button:{tag:"a",className:"small"}}}});a.ext.buttons.collection.className="buttons-collection dropdown";return a.Buttons});
